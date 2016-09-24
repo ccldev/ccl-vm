6ca243711cbdfd67e3f88b238edc0e7e410e3ac0
NetLamp <oplpublishergerman@gmail.com>
2016-09-24 12:54:56 +0200

    Added Number Operations (add,sub,mul,div,gtr,lss)

--- src/ccl/vm/core/expr/NumberExpression.java
+++ src/ccl/vm/core/expr/NumberExpression.java
@@ -30,6 +30,66 @@ public class NumberExpression<T> extends Expression<T> {
 			}
 			
 		}));
+		setProperty("add", new FunctionExpression(new IFunction<Object, Object>(){
+
+			@Override
+			public IExpression<? extends Object> invoke(IExpression<? extends Object>... parameters)
+					throws CclException {
+					return new FloatExpression(
+						(Double) NumberExpression.this.getValue() + (Double) parameters[0].getValue());
+			}
+			
+		}));
+		setProperty("mul", new FunctionExpression(new IFunction<Object, Object>(){
+
+			@Override
+			public IExpression<? extends Object> invoke(IExpression<? extends Object>... parameters)
+					throws CclException {
+					return new FloatExpression(
+						(Double) NumberExpression.this.getValue() * (Double) parameters[0].getValue());
+			}
+			
+		}));
+		setProperty("sub", new FunctionExpression(new IFunction<Object, Object>(){
+
+			@Override
+			public IExpression<? extends Object> invoke(IExpression<? extends Object>... parameters)
+					throws CclException {
+					return new FloatExpression(
+						(Double) NumberExpression.this.getValue() - (Double) parameters[0].getValue());
+			}
+			
+		}));
+		setProperty("div", new FunctionExpression(new IFunction<Object, Object>(){
+
+			@Override
+			public IExpression<? extends Object> invoke(IExpression<? extends Object>... parameters)
+					throws CclException {
+					return new FloatExpression(
+						(Double) NumberExpression.this.getValue() / (Double) parameters[0].getValue());
+			}
+			
+		}));
+		setProperty("lss", new FunctionExpression(new IFunction<Object, Object>(){
+
+			@Override
+			public IExpression<? extends Object> invoke(IExpression<? extends Object>... parameters)
+					throws CclException {
+					return new BooleanExpression(
+						(Double) NumberExpression.this.getValue() < (Double) parameters[0].getValue());
+			}
+			
+		}));
+		setProperty("gtr", new FunctionExpression(new IFunction<Object, Object>(){
+
+			@Override
+			public IExpression<? extends Object> invoke(IExpression<? extends Object>... parameters)
+					throws CclException {
+					return new BooleanExpression(
+						(Double) NumberExpression.this.getValue() > (Double) parameters[0].getValue());
+			}
+			
+		}));
 	}
 	
 }
